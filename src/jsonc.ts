b1d2577715e5d5bf50052cceb6cadaf0bc4d097a
Kevin Goslar <kevin.goslar@gmail.com>
2025-02-27 19:27:32 -0600

    .eslintrc.js tests.yml package.json jsonc.ts jsonc.test.ts tsconfig.json

--- src/jsonc.ts
+++ src/jsonc.ts
@@ -1,42 +1,42 @@
-import fs from "fs"
+import fs from "fs";
 
 /// converts the given JSONC text to valid JSON by stripping all comments
 export function stripComments(text: string): string {
-  let result = ""
-  let in_string = false
+  let result = "";
+  let in_string = false;
   for (const line of text.split("\n")) {
-    let last_char = null
+    let last_char = null;
     for (const cur_char of line) {
-      if (last_char !== "\\" && cur_char === "\"") {
-        in_string = !in_string
+      if (last_char !== "\\" && cur_char === '"') {
+        in_string = !in_string;
       }
       if (!in_string && last_char === "/" && cur_char === "/") {
         // found the beginning of a line comment
-        last_char = null
-        break
+        last_char = null;
+        break;
       }
       if (last_char != null) {
-        result = result + last_char
+        result = result + last_char;
       }
-      last_char = cur_char
+      last_char = cur_char;
     }
     // the loop above doesn't add the last character of the line to the result,
     // let's do that here
     if (last_char != null) {
-      result = result + last_char
+      result = result + last_char;
     }
-    result = result.trim()
-    result = result + "\n"
+    result = result.trim();
+    result = result + "\n";
   }
-  return result.substr(0, result.length - 1)
+  return result.substr(0, result.length - 1);
 }
 
 /// parses the given JSONC text into a JS object
 export function parse(text: string): any {
-  return JSON.parse(stripComments(text))
+  return JSON.parse(stripComments(text));
 }
 
 /// reads a JS object from the file with the given name
 export function loadSync(filePath: string): string {
-  return parse(fs.readFileSync(filePath, "utf-8"))
+  return parse(fs.readFileSync(filePath, "utf-8"));
 }
--- test/jsonc.test.ts
+++ test/jsonc.test.ts
@@ -1,42 +1,42 @@
-import assert from "assert"
-import * as fs from "fs"
-import * as jsonc from "../src/jsonc"
+import assert from "assert";
+import * as fs from "fs";
+import * as jsonc from "../src/jsonc";
 
-test("stripComments", function() {
+test("stripComments", function () {
   const give = `
 // comment 1
 {
   "a": 1, // comment 2
   "url": "https://acme.com",  // contains // inside a string
-}`
+}`;
   const want = `{
   "a": 1,
   "url": "https://acme.com",
-}`
-  const have = jsonc.stripComments(give)
-  assert.equal(have, want)
-})
+}`;
+  const have = jsonc.stripComments(give);
+  assert.equal(have, want);
+});
 
-test("parse", function() {
+test("parse", function () {
   const give = `
 // comment 1
 {
   "a": 1 // comment 2
-}`
-  const want = { "a": 1 }
-  const have = jsonc.parse(give)
-  assert.deepEqual(have, want)
-})
+}`;
+  const want = { a: 1 };
+  const have = jsonc.parse(give);
+  assert.deepEqual(have, want);
+});
 
-test("loadSync", function() {
+test("loadSync", function () {
   const give = `
 // comment 1
 {
   "a": 1 // comment 2
-}`
-  fs.writeFileSync("foo.jsonc", give)
-  const want = { "a": 1 }
-  const have = jsonc.loadSync("foo.jsonc")
-  fs.unlinkSync("foo.jsonc")
-  assert.deepEqual(have, want)
-})
+}`;
+  fs.writeFileSync("foo.jsonc", give);
+  const want = { a: 1 };
+  const have = jsonc.loadSync("foo.jsonc");
+  fs.unlinkSync("foo.jsonc");
+  assert.deepEqual(have, want);
+});
