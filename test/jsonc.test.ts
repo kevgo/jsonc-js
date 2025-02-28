67ec56560660e8115e1d25228f9f0c4f85579e27
Kevin Goslar <kevin.goslar@gmail.com>
2025-02-27 20:18:40 -0600

    jsonc.test.ts

--- test/jsonc.test.ts
+++ test/jsonc.test.ts
@@ -1,5 +1,5 @@
-import assert from "node:assert/strict";
-import { describe, it } from "node:test";
+import assert from "node:assert/strict"
+import { describe, it } from "node:test"
 import * as fs from "fs"
 import * as jsonc from "../src/jsonc"
 
