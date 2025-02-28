bc6291968f442e3e75f4e96228b7864b24ade660
Kevin Goslar <kevin.goslar@gmail.com>
2025-02-28 12:28:33 -0600

    Improve file name (#19)

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,7 @@
 import assert from "node:assert/strict"
 import { test } from "node:test"
 import * as fs from "fs"
-import * as jsonc from "../src/jsonc"
+import * as jsonc from "../src/index"
 
 test("stripComments", () => {
   const give = `
