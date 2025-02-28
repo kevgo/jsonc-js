655c4e38c55f77dda22615835479d2c5d8c8c09f
Kevin Goslar <kevin.goslar@gmail.com>
2025-02-28 13:09:38 -0600

    Fix comments

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { promises as fs } from "fs"
 
-/// converts the given JSONC text to valid JSON by stripping all comments
+/** converts the given JSONC text to valid JSON by stripping all comments */
 export function strip(text: string): string {
   let result = ""
   let in_string = false
@@ -28,12 +28,12 @@ export function strip(text: string): string {
   return result.substring(0, result.length - 1)
 }
 
-/// parses the given JSONC text into a JS object
+/** parses the given JSONC text into a JS object */
 export function parse(text: string): any {
   return JSON.parse(strip(text))
 }
 
-/// reads a JS object from the file with the given name
+/** reads a JS object from the file with the given name */
 export async function load(filePath: string): Promise<string> {
   return parse(await fs.readFile(filePath, "utf-8"))
 }
