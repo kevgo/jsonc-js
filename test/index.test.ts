b8cf51bbb9eacb29b650eebd10aa7f2c18a353f8
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-19 16:43:21 -0500

    Maintain whitespace for correct line and column numbers (#40)

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,7 @@
 import * as fs from "fs"
 import assert from "node:assert/strict"
 import { test } from "node:test"
+import * as util from "node:util"
 import * as jsonc from "../src/index"
 
 test("stripComments", () => {
@@ -8,14 +9,20 @@ test("stripComments", () => {
 // comment 1
 {
   "a": 1, // comment 2
-  "url": "https://acme.com",  // contains // inside a string
+  "url": "https://acme.com", // contains // inside a string
 }`
-  const want = `{
-  "a": 1,
-  "url": "https://acme.com",
+  const want = `
+
+{
+  "a": 1, \n\
+  "url": "https://acme.com", \n\
 }`
   const have = jsonc.strip(give)
-  assert.equal(have, want)
+  if (have !== want) {
+    console.log("have:", util.inspect(have))
+    console.log("want:", util.inspect(want))
+    assert.fail("mismatching strings, see output above")
+  }
 })
 
 test("parse", () => {
